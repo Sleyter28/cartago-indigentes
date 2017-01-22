23e3b00c1e2df8579444f1e1e089de7bc6182fcc
Sleyter A <sleyterangulo@Sleyters-MacBook-Pro.local>
2017-01-21 21:38:42 -0600

    New

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,12 @@
-$('.topnav li').click(function(){
-    $(this).addClass('active').siblings().removeClass('active');
-})
+$(document).ready(function () {
+    $('.topnav li a').click(function(e) {
+
+        $('.topnav li').removeClass('active');
+
+        var $parent = $(this).parent();
+        if (!$parent.hasClass('active')) {
+            $parent.addClass('active');
+        }
+        e.preventDefault();
+    });
+});
