f760a2c334670df98636d86bf3bea8f697417330
Ariel Montero Monestel <arielmmonestel@gmail.com>
2017-01-25 10:01:45 -0600

    completa ventana de grupos, avance ventana de actividades

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,107 @@ function cargar(){
 	
 }
 
+function agregarObjetivo(){
+	var divObjetivos=document.getElementById("lista_de_objetivos");
+	var max = 10000;
+	var min = 15;
+	var id=  Math.floor(Math.random()* (max-min+1))+min;
+	var dd= document.createElement("dd");
+	var input = document.createElement("input");
+	input.type="text";
+	input.id= id;
+	var button=document.createElement("button");
+	button.id="button"+id;
+	var m= document.createElement("i");
+	var attribute = document.createAttribute("class");
+	attribute.value = "fa fa-times";
+	m.setAttributeNode(attribute);
+	var attribute = document.createAttribute("aria-hidden");
+	attribute.value = "true";
+	m.setAttributeNode(attribute);
+	var attribute = document.createAttribute("onclick");
+	attribute.value = "borrarObjetivo("+id+")";
+	button.setAttributeNode(attribute);
+	button.appendChild(m);
+	dd.appendChild(input);
+	dd.appendChild(button);
+	divObjetivos.appendChild(dd);
+}
+function GuardarCambios(){
+	var input_nombre= document.getElementById("nombre_grupo_trabajo");
+	var select= document.getElementById("grupos_trabajo");
+	var option= select.value;
+	var nombre = input_nombre.value;
+	var input_resumen = document.getElementById("resumen_grupo_trabajo");
+	var resumen = input_resumen.value;
+	var lista_de_dd= document.getElementById("lista_de_objetivos").childNodes;
+	var lista_de_objetivos= Array(1000);
+	for (var i = 0; i < lista_de_dd.length ; i++) {
+		var lista_de_inputs=lista_de_dd[i].childNodes;
+		for (var j =0; j<lista_de_inputs.length;j++) {
+			if(lista_de_inputs[j].tagName.toUpperCase() == "INPUT" ){
+				lista_de_objetivos.push(lista_de_inputs[j].value);
+			}
+		}
+		
+	}
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	      	var response= xhttp.responseText; //1 si borro, 2 no borro
+	      	if (response=="1") {
+	      		alert("Grupo actualizado correctamente");
+	      	}else{	
+	      		alert("No se pudo actualizar el grupo");
+	      	}
+	      	
+	    }
+	  
+	};
+	var data = new FormData();
+	data.append('id', '4');
+	data.append('grupo', option);
+	data.append('nombre', nombre);
+	data.append('resumen', resumen);
+	data.append('objetivos', JSON.stringify(lista_de_objetivos));
+	xhttp.open("POST","http://ic-itcr.ac.cr/~gubaldo/proyecto/php/loader.php", true);
+	xhttp.send(data);
+	/*revisar lo de los espacios en blanco, tal vez es necesario cambiar de metodo por post para poder enviar espacios en blanco!**/
+
+}
+function EliminarGrupo(){
+	var select = document.getElementById("delete_grupo_trabajo");
+	var option = select.value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	      	var response= xhttp.responseText; //1 si borro, 2 no borro
+	      	if (response=="1") {
+	      		alert("Grupo correctamente eliminado");
+	      		select.remove(select.selectedIndex);
+	      		select.selectedIndex="0";
+	      	}else{	
+	      		alert("No se pudo eliminar el grupo");
+	      	}
+	      	
+	    }
+	  
+	};
+	xhttp.open("GET","http://ic-itcr.ac.cr/~gubaldo/proyecto/php/loader.php?id=3&grupo="+option, true);
+	xhttp.send();
+	
+
+}
+
+function borrarObjetivo(id){
+	var element = document.getElementById(id);
+	  element.parentNode.removeChild(element);
+	var button = document.getElementById("button"+id);
+	  button.parentNode.removeChild(button);
+}
 function cargarInput(){
 	var value= document.getElementById("grupos_trabajo").value;
 	var xhttp = new XMLHttpRequest();
@@ -17,13 +118,18 @@ function cargarInput(){
 			nombre.value=objs.nombre;
 			resumen.value=objs.resumen;
 			var divObjetivos=document.getElementById("lista_de_objetivos");
+			while (divObjetivos.firstChild) {
+				divObjetivos.removeChild(divObjetivos.firstChild);
+			}
 		  	for (var i= 0; i<objs.objetivos.length; i++) {
 		  		/**<dd><input type="text" name="objetivo_grupo_trabajo"><button><i class="fa fa-times" aria-hidden="true"></i></button></dd>*/
 		  		var dd= document.createElement("dd");
 		  		var input = document.createElement("input");
 		  		input.type="text";
 		  		input.name= objs.objetivos[i]["idobjetivo"];
+		  		input.id= objs.objetivos[i]["idobjetivo"];
 		  		var button=document.createElement("button");
+		  		button.id="button"+objs.objetivos[i]["idobjetivo"];
 		  		var m= document.createElement("i");
 		  		var attribute = document.createAttribute("class");
 				attribute.value = "fa fa-times";
@@ -31,6 +137,9 @@ function cargarInput(){
 				var attribute = document.createAttribute("aria-hidden");
 				attribute.value = "true";
 				m.setAttributeNode(attribute);
+				var attribute = document.createAttribute("onclick");
+				attribute.value = "borrarObjetivo("+objs.objetivos[i]["idobjetivo"]+")";
+				button.setAttributeNode(attribute);
 		  		button.appendChild(m);
 		  		input.value=objs.objetivos[i]["objetivo"];
 		  		dd.appendChild(input);
@@ -48,6 +157,7 @@ function cargarInput(){
 
 function cargarSelectEditGrupo(){		
 	var select=document.getElementById("grupos_trabajo");
+	var select_delete=document.getElementById("delete_grupo_trabajo");
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 	    if (this.readyState == 4 && this.status == 200) {
@@ -56,9 +166,14 @@ function cargarSelectEditGrupo(){
 	      	var objs = JSON.parse(json);
 		  	for (var i= 0; i<objs.length; i++) {
 		  		var new_option = document.createElement("option");
+		  		var second_new_option =  document.createElement("option");
 				new_option.innerHTML= objs[i]["nombre"];
 				new_option.value=objs[i]["idgrupoTrabajo"];
+
+				second_new_option.innerHTML= objs[i]["nombre"];
+				second_new_option.value=objs[i]["idgrupoTrabajo"];
 				select.appendChild(new_option);
+				select_delete.appendChild(second_new_option);
 
 		  	}
 	      	
@@ -69,4 +184,31 @@ function cargarSelectEditGrupo(){
 	xhttp.send();
 	
 	
+}
+
+function cargarActividades(id_select,id_select_to_change){
+	var select= document.getElementById(id_select);
+	var value_select= select.value;
+	var select_to_change=document.getElementById(id_select_to_change);
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	      	var json= xhttp.responseText;
+	      	var objs = JSON.parse(json);
+		  	for (var i= 0; i<objs.length; i++) {
+		  		var new_option = document.createElement("option");
+		  		new_option.innerHTML= objs[i]["nombre"];
+				new_option.value=objs[i]["idactividad"];
+				select_to_change.appendChild(new_option);
+				
+		  	}
+	      	
+	    }
+	  
+	};
+	xhttp.open("GET","http://ic-itcr.ac.cr/~gubaldo/proyecto/php/loader.php?id=4&grupo="+value_select, true);
+	xhttp.send();
+	
+
 }
--- php/loader.php
+++ php/loader.php
@@ -1,65 +1,139 @@
 <?php
 include("database.php");
-$option = $_GET['id']; // id de la operación que se va a realizar
-switch ($option) {
-	case 1:
-		# se traerá a todos los grupos de trabajo...
-		$sql="SELECT idgrupoTrabajo,nombre FROM grupoTrabajo;";
-		$result = $conn->query($sql);
-		$arraylist = array();
-		if ($result->num_rows > 0) {
-		    // output data of each row
-		    while($row = $result->fetch_assoc()) {
-		    	array_push($arraylist,array("idgrupoTrabajo"=>$row["idgrupoTrabajo"],"nombre"=>$row["nombre"]));
-		    }
-		   	echo json_encode($arraylist);    
-		}
-		else {
-		    echo "0";
-		}
-		$conn->close();
-		break;
-	case 2:
-		# se traerá a todos los grupos de trabajo...
-		
-		$arraylist = array();
-		$grupo_id= $_GET['grupo'];
-		$sql="SELECT idgrupoTrabajo,nombre,resumen FROM grupoTrabajo where idgrupoTrabajo=$grupo_id;";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-		    // output data of each row
-		    
-		    while($row = $result->fetch_assoc()) {
-		    	$arraylist["idgrupoTrabajo"]=$row["idgrupoTrabajo"];
-		    		$arraylist["nombre"]=$row["nombre"];
-		    		$arraylist["resumen"]=$row["resumen"];
+	if (isset($_GET['id'])) {
+		$option_get = $_GET['id']; // id de la operación que se va a realizar
+		switch ($option_get) {
+			case 1:
+				# se traerá a todos los grupos de trabajo...
+				$sql="SELECT idgrupoTrabajo,nombre FROM grupoTrabajo;";
+				$result = $conn->query($sql);
+				$arraylist = array();
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				    	array_push($arraylist,array("idgrupoTrabajo"=>$row["idgrupoTrabajo"],"nombre"=>$row["nombre"]));
+				    }
+				   	echo json_encode($arraylist);    
+				}
+				else {
+				    echo "0";
+				}
+				$conn->close();
+				break;
+			case 2:
+				# se traerá a todos los grupos de trabajo...
+				
+				$arraylist = array();
+				$grupo_id= $_GET['grupo'];
+				$sql="SELECT idgrupoTrabajo,nombre,resumen FROM grupoTrabajo where idgrupoTrabajo=$grupo_id;";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    
+				    while($row = $result->fetch_assoc()) {
+				    	$arraylist["idgrupoTrabajo"]=$row["idgrupoTrabajo"];
+				    		$arraylist["nombre"]=$row["nombre"];
+				    		$arraylist["resumen"]=$row["resumen"];
 
-		    }
-		}
-			/*
-`grupoTrabajo_idgrupoTrabajo``objetivo``idobjetivo`*/
-		
-		$sql="SELECT idobjetivo,objetivo FROM objetivo where grupoTrabajo_idgrupoTrabajo = $grupo_id;";
-		$result = $conn->query($sql);
-		$objetivos =array();
-		if ($result->num_rows > 0) {
-		    // output data of each row
-		    
-		    while($row = $result->fetch_assoc()) {
-		    	array_push($objetivos,array("idobjetivo"=>$row["idobjetivo"],"objetivo"=>$row["objetivo"]));
+				    }
+				}
+					/*
+			grupoTrabajo_idgrupoTrabajoobjetivoidobjetivo*/
+				
+				$sql="SELECT idobjetivo,objetivo FROM objetivo where grupoTrabajo_idgrupoTrabajo = $grupo_id;";
+				$result = $conn->query($sql);
+				$objetivos =array();
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    
+				    while($row = $result->fetch_assoc()) {
+				    	array_push($objetivos,array("idobjetivo"=>$row["idobjetivo"],"objetivo"=>$row["objetivo"]));
 
-		    }
-		    $arraylist["objetivos"]=$objetivos;
+				    }
+				    $arraylist["objetivos"]=$objetivos;
 
+				}
+				
+				echo json_encode($arraylist);    
+				$conn->close();
+				break;
+			case 3:
+				# se traerá a todos los grupos de trabajo...
+				
+				$arraylist = array();
+				$grupo_id= $_GET['grupo'];
+				$retorno=0;
+				$sql="DELETE FROM objetivo where grupoTrabajo_idgrupoTrabajo = $grupo_id;";
+				if ($result = $conn->query($sql)) {
+					$retorno =1;
+				}
+				
+				$sql="DELETE FROM grupoTrabajo where idgrupoTrabajo=$grupo_id;";
+				if ($result = $conn->query($sql)) {
+					$retorno =1;
+				}
+				echo $retorno;    
+				$conn->close();
+				break;
+			case 4:
+				# se traerá a todos las actividades del grupo de trabajo indicado con el id... idactividad, nombre, fecha, lugar, descripcion, rutaImagen, grupoTrabajo_idgrupoTrabajo
+				
+				$arraylist = array();
+				$grupo_id= $_GET['grupo'];
+				$sql="SELECT * FROM objetivo where grupoTrabajo_idgrupoTrabajo = $grupo_id;";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    
+				    while($row = $result->fetch_assoc()) {
+				    	array_push($arraylist,array("idactividad"=>$row["idactividad"],"nombre"=>$row["nombre"]));
+
+				    }
+
+				}
+				
+				echo json_encode($arraylist);    
+				$conn->close();
+				break;
+
+			default:
+				break;
+			}
+	}
+	else{// si no está seteado el $_GET quiere decir que estaré usando POST
+		$option_post = $_POST['id']; // id de la operación que se va a realizar
+		switch ($option_post) {
+			case 4:
+				# se traerá a todos los grupos de trabajo...
+				
+				$arraylist = array();
+				$grupo_id= $_POST['grupo'];
+				$objetivos= json_decode($_POST['objetivos']);
+				$nombre = $_POST['nombre'];
+				$resumen = $_POST['resumen'];
+				$retorno=0;
+				$sql="UPDATE grupoTrabajo SET nombre='$nombre',resumen='$resumen' WHERE idgrupoTrabajo= $grupo_id;";
+				$result = $conn->query($sql);
+				$retorno =1;
+				$sql="DELETE FROM objetivo WHERE grupoTrabajo_idgrupoTrabajo=$grupo_id;";
+				$result = $conn->query($sql);
+				foreach ($objetivos as $objetivo) {
+					if (strlen($objetivo)>0) {
+					$sql="INSERT INTO objetivo( objetivo, grupoTrabajo_idgrupoTrabajo) VALUES ('$objetivo',$grupo_id);";
+					$result = $conn->query($sql);
+					}
+				}
+				echo $retorno;    
+				$conn->close();
+				break;
+				/*
+					INSERT INTO `actividad`(`idactividad`, `nombre`, `fecha`, `lugar`, `descripcion`, `rutaImagen`, `grupoTrabajo_idgrupoTrabajo`) VALUES (7.8.10.11
+				**/
+			default:
+				# code...
+				break;
 		}
-		
-		echo json_encode($arraylist);    
-		$conn->close();
-		break;
-	
-	default:
-		break;
-}
+		}
 
 
 
